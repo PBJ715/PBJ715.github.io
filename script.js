c8f36f3d26b9d56490ffc830b57dd45b47e07730
PBJ715 <jerry.lyc715@gmail.com>
2024-06-27 19:54:53 -0700

    June 27th commit

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-function darkMode(){
-    document.body.style.backgroundColor="#2c2f33";
-    document.body.style.color="#ffffff";
-    document.body.style.transition="background-color 2s, color 2s";
-    document.getElementById("linkedIn").style.color="#3f83e1";
-    document.getElementById("gitHub").style.color="#3f83e1";
-    document.getElementById("misc").style.color="#3f83e1";
-}
+document.getElementById("bgcolor").addEventListener("click", changeColor);
 
-function lightMode(){
-    document.body.style.backgroundColor="#ffffff";
-    document.body.style.color="#000000";
-    document.body.style.transition="background-color 2s, color 2s";
-    document.getElementById("linkedIn").style.color="#0000EE";
-    document.getElementById("gitHub").style.color="#0000EE";
-    document.getElementById("misc").style.color="#0000EE";
+function changeColor(){
+    if (document.body.className == "dark-mode"){
+        document.body.className = "light-mode";
+    } else if (document.body.className == "light-mode"){
+        document.body.className = "dark-mode";
+    }
 }
 
 function rickroll(){
